559882ec2953d44f128d040ba68962b917ce0bb1
Song <songnan17@qq.com>
2019-05-09 23:08:52 +0800

    Pass LoginTest.should_give_hint_when_username_or_password_is_empty.

--- src/main/java/com/example/login/Login.java
+++ src/main/java/com/example/login/Login.java
@@ -2,5 +2,8 @@ package com.example.login;
 
 public class Login {
     public static void login(String username, String password, String confirmPassword) {
+        if (username.isEmpty() || password.isEmpty()) {
+            System.out.println("用户名和密码不能为空！");
+        }
     }
 }
