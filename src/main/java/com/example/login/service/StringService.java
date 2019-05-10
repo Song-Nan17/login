7f1e0a55d8c6fb37b54edbc9299cd3ed9e02064a
Song <songnan17@qq.com>
2019-05-10 18:00:16 +0800

    Pass LoginTest.should_give_hint_when_password_shorter_than_8.

--- src/main/java/com/example/login/service/StringService.java
+++ src/main/java/com/example/login/service/StringService.java
@@ -1,13 +1,16 @@
 package com.example.login.service;
 
 public class StringService {
-    public static boolean isEmail(String email){
+    public static boolean isEmail(String email) {
         String format = "^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$";
         return email.matches(format);
     }
 
     public static boolean isValidPassword(String password) {
-        String format = "^[a-zA-Z0-9]$";
-        return password.matches(format);
+        String letters = "^.*[a-zA-Z].*$";
+        String numbers = "^.*[0-9].*$";
+        return password.matches(letters) &&
+                password.matches(numbers) &&
+                password.length() >= 8;
     }
 }
